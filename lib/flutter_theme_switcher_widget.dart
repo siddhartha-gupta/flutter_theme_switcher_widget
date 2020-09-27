e6667150e87e85a2cd825f48ad0b99f8cf59c4e8
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2020-09-27 17:22:25 +0530

    exporting widget

--- lib/flutter_theme_switcher_widget.dart
+++ lib/flutter_theme_switcher_widget.dart
@@ -1,7 +1,3 @@
 library flutter_theme_switcher_widget;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/theme_switch_widget.dart';
